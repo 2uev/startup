5c0151d4ab87d684f7621c511fbc5d8ea1cc7d61
2uev <zuevtaras@gmail.com>
2018-11-08 16:45:05 +0500

    test1

---
+++ script.js
@@ -0,0 +1,14 @@
+$(function() {
+    $('#vid1').resizable({
+        alsoResize: "#vid1 video"
+        });
+    $('#vid2').resizable({
+        alsoResize: "#vid2 video"
+        });
+    $('#vid3').resizable({
+        alsoResize: "#vid3 video"
+        });
+    $('#vid4').resizable({
+        alsoResize: "#vid4 video"
+        })
+    });
