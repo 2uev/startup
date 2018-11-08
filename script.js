c1beb49e8f21e08e50cd73b926043163a7a2f3bb
2uev <zuevtaras@gmail.com>
2018-11-08 18:43:19 +0500

    test1_final

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 $(function() {
-    $('#vid1').resizable({
+    $('#vid1').draggable().resizable({
         alsoResize: "#vid1 video"
         });
-    $('#vid2').resizable({
+    $('#vid2').draggable().resizable({
         alsoResize: "#vid2 video"
         });
-    $('#vid3').resizable({
+    $('#vid3').draggable().resizable({
         alsoResize: "#vid3 video"
         });
-    $('#vid4').resizable({
+    $('#vid4').draggable().resizable({
         alsoResize: "#vid4 video"
-        })
+       })
     });
