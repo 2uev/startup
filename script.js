666a26daf38ac1f6f6f48851b5a3fefc1b122842
2uev <zuevtaras@gmail.com>
2018-11-09 00:05:57 +0500

    add flex

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 $(function() {
     $('#vid1').draggable().resizable({
-        alsoResize: "#vid1 video"
+        alsoResize: "#vid1 video",
+        handles: "all",
+        grid: [50, 50]
         });
     $('#vid2').draggable().resizable({
-        alsoResize: "#vid2 video"
+        alsoResize: "#vid2 video",
+        handles: "all",
+        grid: [50, 50]
         });
     $('#vid3').draggable().resizable({
-        alsoResize: "#vid3 video"
+        alsoResize: "#vid3 video",
+        handles: "all",
+        grid: [50, 50]
         });
     $('#vid4').draggable().resizable({
-        alsoResize: "#vid4 video"
+        alsoResize: "#vid4 video",
+        handles: "all",
+        grid: [50, 50]
        })
     });
